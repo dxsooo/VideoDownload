84fd641dee6e0a18eec3d2ac1ec362db1390a3ba
dxsooo <zeohan.dxs@gmail.com>
2022-08-23 10:17:53 +0800

    test unavailable urls

--- send_celery_task.py
+++ send_celery_task.py
@@ -2,14 +2,16 @@ from celery_worker import download
 from config import DOWNLOAD_DIR
 
 test_urls = [
-    # "https://www.youtube.com/watch?v=sHFsq-BIhWs",
+    "https://www.youtube.com/watch?v=sHFsq-BIhWs",
+    "https://www.youtube.com/watch?v=_wO522Dgtrk",
+    "https://www.youtube.com/watch?v=pBxN2GjY_08",  # Video unavailable
     "https://www.bilibili.com/video/BV1be4y1X7sq",
     "https://www.bilibili.com/video/BV1NF411P7C9",
-    "https://www.bilibili.com/video/BV1Ur4y1G7en",  # 失效视频
+    "https://www.bilibili.com/video/BV1Ur4y1G7en",  # Video unavailable
 ]
 
 if __name__ == "__main__":
     for i in test_urls:
         download.delay(i, DOWNLOAD_DIR)
         # download.delay(i)
-        # download.apply_async((i, DOWNLOAD_DIR), queue="bilibili")
+        # download.apply_async((i, DOWNLOAD_DIR), queue="youtube")
