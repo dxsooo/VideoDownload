82bb877d0f450908a2c6c8c6e8a275cbc453b29a
dxsooo <zeohan.dxs@gmail.com>
2022-10-21 15:37:47 +0800

    add ixigua support

---
+++ download_ixigua.py
@@ -0,0 +1,21 @@
+import os
+import re
+
+from you_get import common
+
+
+def is_ixigua_video(url: str) -> bool:
+    return None != re.match("^https:\/\/www.ixigua.com\/[0-9]+$", url)
+
+
+def download_ixigua_video(url: str, dir: str):
+    os.makedirs(dir, exist_ok=True)
+    common.output_filename = get_id(url)
+    # print(common.output_filename)
+    common.any_download(url=url, info_only=False, output_dir=dir, merge=True)
+
+
+def get_id(url: str) -> str:
+    se = re.search(r"([0-9]+)$", url)
+    if se != None:
+        return se.group(0)
