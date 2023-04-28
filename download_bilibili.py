7dcf38fad762d967fead86c6fdc10f80ee93229c
dxsooo <zeohan.dxs@gmail.com>
2023-04-28 17:44:46 +0800

    upgrade bilix

--- download_bilibili.py
+++ download_bilibili.py
@@ -3,21 +3,21 @@ import glob
 import os
 import re
 
-import bilix.api.bilibili as api
-from bilix import DownloaderBilibili
+import bilix.sites.bilibili.api as api
+from bilix.sites.bilibili.downloader import DownloaderBilibili
 
 
 def is_bilibili_video(url: str) -> bool:
-    if None != re.match("^https:\/\/www.bilibili.com\/video\/BV[a-zA-z0-9]+$", url):
+    if None != re.match(r"^https://www.bilibili.com/video/BV[a-zA-z0-9]+$", url):
         return True
     else:
-        return None != re.match("^http:\/\/www.bilibili.com\/video\/av[0-9]+$", url)
+        return None != re.match(r"^http://www.bilibili.com/video/av[0-9]+$", url)
 
 
 def download_bilibili_video(url: str, dir: str):
     os.makedirs(dir, exist_ok=True)
     asyncio.run(
-        _download_video(url, DownloaderBilibili(part_concurrency=10, videos_dir=dir))
+        _download_video(url, DownloaderBilibili(part_concurrency=10), videos_dir=dir)
     )
 
 
@@ -29,7 +29,7 @@ def get_id(url: str) -> str:
         return re.search(r"(av[0-9]+)$", url).group(0)
 
 
-async def _download_video(url, bdl):
+async def _download_video(url, bdl, videos_dir):
     # get bvid/avid from url to rename later
     name = get_id(url)
     try:
@@ -38,17 +38,17 @@ async def _download_video(url, bdl):
         # print(video_info.pages[video_info.p].p_name)
         video_info.pages[video_info.p].p_name = name
 
-        await bdl.get_video(url, video_info=video_info)
-        out_vs = glob.glob(os.path.join(bdl.videos_dir, "*-" + name + ".mp4"))
+        await bdl.get_video(url, path=videos_dir, video_info=video_info)
+        out_vs = glob.glob(os.path.join(videos_dir, "*-" + name + ".mp4"))
         if len(out_vs) == 0:
             raise Exception(f"Failed to get video in {url}")
         os.rename(
             out_vs[0],
-            os.path.join(bdl.videos_dir, name + ".mp4"),
+            os.path.join(videos_dir, name + ".mp4"),
         )
     except Exception:
         # clean
-        out_vs = glob.glob(os.path.join(bdl.videos_dir, "*-" + name + "*"))
+        out_vs = glob.glob(os.path.join(videos_dir, "*-" + name + "*"))
         for o in out_vs:
             os.remove(o)
         raise
